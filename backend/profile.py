58b77fc0009e7f84df05656ccdcae40cf296d3e9
Prasad Kute <prasadkute0708@gmail.com>
2024-04-15 23:46:49 +0530

    final changes

--- backend/profile.py
+++ backend/profile.py
@@ -8,7 +8,7 @@ images_folder = "/user_images"  # Replace with your images folder path
 @profile_bp.route('/view_profile/<username>')
 def index(username): #Displays the profile page for a specific user.
     data = view_profile_data(username)
-    print(data)
+    # print(data)
     return render_template('profiles/view_profile.html', data = data, username = username)
     
 @profile_bp.route('/edit_profile', methods=['GET'])
@@ -22,25 +22,11 @@ def edit_profile():
 @profile_bp.route('/update_profile', methods=['POST'])
 def update_profile():
     # global user_type
-    # user = session['user']
+  
     username = session['username']
+    # print(request.form)
 
-    # image_file = request.files["profile_pic"]
-    # image_path = request.form['image_path']
-    # print(request.files)  # Added for debugging
-
-    # if username and image_file:
-    #     # print("enter this if condition")
-    #     # Construct filename using username and extension
-    #     filename = f"{username}.jpg"  # Adjust extension as needed
-    #     # Construct image path only if there's an image file
-    #     image_path = os.path.join(images_folder, filename)
-
-    #     # Save image
-    #     image_file.save(image_path)
-    # print("usr ty 3 : " + user_type)
-    # print("user type : " + user_type + " username  :" + username + "request.form : ")
-    update_profile_database( username, request.form)
+    update_profile_database(username, request.form)
     data = view_profile_data(username)
     # return redirect(url_for('profile.index', username=username))
     return render_template('profiles/view_profile.html', data = data, username = username)
